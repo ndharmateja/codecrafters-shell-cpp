beaaf638035f0048e36d3ef3730caa47ee726dcd
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-06-03 20:41:40 +0530

    refactor to use the CommandParser & Command in the main loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include "commands/command_parser.hpp"
 
 int main()
 {
@@ -8,34 +9,15 @@ int main()
     std::cerr << std::unitbuf;
 
     // Loop the prompt and take input
-    std::string command;
+    std::string cmd_str;
     while (true)
     {
-        // Print the $ prompt
+        // Print the $ prompt and get the command string input
         std::cout << "$ ";
+        std::getline(std::cin, cmd_str);
 
-        // Get the command input and print invalid commands
-        std::getline(std::cin, command);
-
-        // Handle exit command
-        if (command == "exit")
-            break;
-
-        // Handle echo command
-        if (command.starts_with("echo"))
-            std::cout << command.substr(5) << std::endl;
-
-        // Handle type command
-        else if (command.starts_with("type"))
-        {
-            std::string_view arg_string = std::string_view(command).substr(5);
-            if (arg_string == "type" || arg_string == "echo" || arg_string == "exit")
-                std::cout << arg_string << " is a shell builtin" << std::endl;
-            else
-                std::cout << arg_string << ": not found" << std::endl;
-        }
-
-        else
-            std::cout << command << ": command not found" << std::endl;
+        // Parse the command and execute it
+        std::unique_ptr<Command> cmd = CommandParser::parse_command(cmd_str);
+        cmd->execute();
     }
 }
