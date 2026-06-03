3c92d40db927a9333c95ecb63310ff94df6ba7b2
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2026-06-03 16:33:49 +0530

    add loop for the command prompt

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,15 @@ int main()
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
 
-    // Print the $ prompt
-    std::cout << "$ ";
-
-    // Get the command input and print invalid commands
+    // Loop the prompt and take input
     std::string command;
-    std::getline(std::cin, command);
-    std::cout << command << ": command not found" << std::endl;
+    while (true)
+    {
+        // Print the $ prompt
+        std::cout << "$ ";
+
+        // Get the command input and print invalid commands
+        std::getline(std::cin, command);
+        std::cout << command << ": command not found" << std::endl;
+    }
 }
